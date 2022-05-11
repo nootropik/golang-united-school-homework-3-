c317fcb5550ff898fb4e5885bf39b37b838911f8
Denis Kuulyga <nootropik@gmail.com>
2022-05-11 18:38:30 +0300

    task 1

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,10 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+	var sum float32 = 0
+	for i := 0; i < 15; i++ {
+		sum += (input[i])
+	}
+	result = sum / 15
+	return result
 }
