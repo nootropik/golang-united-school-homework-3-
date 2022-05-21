85e87e0895016229177e4eaf6bbd5d76bfeb134c
Denis Kuulyga <nootropik@gmail.com>
2022-05-21 22:39:09 +0300

    task 2

--- task02-slice.go
+++ task02-slice.go
@@ -2,10 +2,10 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	result := make([]int64, len(input), cap(input))
+	result = make([]int64, len(input), cap(input))
 	copy(result, input)
 	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
 		result[i], result[j] = result[j], result[i]
 	}
-
+	return result
 }
